385a76337297c7f1cf837858fb1998d4c35e67e3
Wolfleader <james.morgan@iinet.net.au>
2021-05-03 09:30:52 +0800

    overhaul of movement and hovering

--- Assets/Scripts/Hovering.cs
+++ Assets/Scripts/Hovering.cs
@@ -2,13 +2,14 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.InputSystem;
 
 public class Hovering : MonoBehaviour
 {
     [SerializeField] private float hoverRate = 10f;
     public float HoverRate => hoverRate;
 
-    [SerializeField] [Range(0, 1)] private float hoverRange = 1;
+    [SerializeField] [Range(0, 1)] private float hoverRange = 0.25f;
     public float HoverRange => hoverRange;
 
     [SerializeField] private float upperHeight = 2f;
@@ -17,15 +18,33 @@ public class Hovering : MonoBehaviour
     [SerializeField] float lowerHeight = 1.2f;
     public float LowerHeight => lowerHeight;
 
+    private Vector2 _input;
+    private float _speed;
+
     // Start is called before the first frame update
     void Start()
     {
-        
+        _speed = GetComponent<Movement>().Speed;
     }
 
     // Update is called once per frame
     void Update()
     {
+        // TODO
+        /*
+         * Make lower height decrease when going down
+         * Make Upper height decrease when going down
+         * make lower height increase when going up
+         *
+         * Fix overall hovering system
+         * 
+         */
+        if (_input.y < 0)
+        {
+            upperHeight += _input.y * (_speed * Time.deltaTime);
+        }
+        // EVENTUALLY USE THIS TO CHANGE LOWER HEIGHT
+        //lowerHeight += _input.y * (_speed * Time.deltaTime);
         transform.position = ClampHeight((Vector3.up * Mathf.Cos(Time.time * hoverRate) * ClampRange(hoverRange)) + transform.position);
     }
     
@@ -55,5 +74,10 @@ public class Hovering : MonoBehaviour
         if (value.y > upperHeight)
             value.y = upperHeight;
         return value;
+    }
+
+    public void Move(InputAction.CallbackContext ctx)
+    {
+        _input = ctx.ReadValue<Vector2>();
     }
 }
--- Assets/Scripts/Movement.cs
+++ Assets/Scripts/Movement.cs
@@ -4,27 +4,31 @@ using UnityEngine;
 using UnityEngine.InputSystem;
 
 public class Movement : MonoBehaviour
-{ 
+{
     [SerializeField] private float speed = 10f;
     public float Speed => speed;
-    
+
+    [SerializeField] private bool flyingType = false;
+    public bool FlyingType => flyingType;
+
     [SerializeField] private float jumpHeight = 5f;
     public float JumpHeight => jumpHeight;
 
     [SerializeField] private BoxCollider2D floor;
     public BoxCollider2D Floor => floor;
-    
+
     [SerializeField] private bool grounded = false;
     public bool Grounded => grounded;
-    
-    
+
+
     private Rigidbody2D _rb;
     private Vector2 _input;
     private bool _canJump = false;
-    
+
     // Start is called before the first frame update
     void Start()
     {
+        if (flyingType) return;
         _canJump = true;
         _rb = GetComponent<Rigidbody2D>();
     }
@@ -32,31 +36,35 @@ public class Movement : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        
-        transform.position += new Vector3(_input.x, 0, _input.y) * (speed * Time.deltaTime);
+        if(flyingType) transform.position += new Vector3(_input.x, _input.y, 0) * (speed * Time.deltaTime);
+        if(!flyingType) transform.position += new Vector3(_input.x, 0, 0) * (speed * Time.deltaTime);
     }
 
     private void FixedUpdate()
     {
+        if (flyingType) return;
+        
         if (_input.y > 0 && _canJump)
         {
             _rb.velocity = new Vector2(_rb.velocity.x, jumpHeight);
             _canJump = false;
         }
-        
+
         _rb.velocity = new Vector2(0, _rb.velocity.y);
     }
+
     void OnCollisionEnter2D(Collision2D col)
     {
         if (col.gameObject.CompareTag("Floor"))
         {
             grounded = true;
         }
+
         if (col.gameObject.CompareTag("Player") && col.gameObject.GetComponent<Movement>().grounded)
-        { 
+        {
             _canJump = true;
         }
-        
+
         if (grounded)
         {
             _canJump = true;
@@ -70,11 +78,9 @@ public class Movement : MonoBehaviour
             grounded = false;
         }
     }
-    
+
     public void Move(InputAction.CallbackContext ctx)
     {
         _input = ctx.ReadValue<Vector2>();
     }
-
 }
-
